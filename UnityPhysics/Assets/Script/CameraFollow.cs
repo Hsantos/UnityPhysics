50fb6d46c334f472be2bb36c65962b7372b36a4e
Hsantos <igor.hos@gmail.com>
2018-12-18 21:09:20 -0200

    dev

---
+++ UnityPhysics/Assets/Script/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public float offsetX = 0f;
+    public float offsetY = 5f;
+    public float offsetZ = -15f;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+      Vector3 targetPosition = new Vector3(target.transform.localPosition.x+offsetX, target.transform.localPosition.y+offsetY, target.transform.localPosition.z+offsetZ);
+	  transform.localPosition = targetPosition;
+        
+	}
+}
---
+++ UnityPhysics/Assets/Script/Spaceship.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Spaceship : MonoBehaviour
+{
+    public float height = 10f;
+    public float calcForce;
+    public Rigidbody rigidBody;
+    public float proportionalHeight;
+    public Vector3 destination;
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate () {
+
+	    Debug.DrawRay(transform.position, Vector3.down * height, Color.red);
+
+	    RaycastHit ht;
+	    Ray landingRay = new Ray(transform.position, Vector3.down);
+
+	    if (Physics.Raycast(landingRay, out ht, height))
+	    {
+	        if (ht.transform.gameObject.GetComponent<TerrainControl>())
+	        {
+	            proportionalHeight = (height - ht.distance);
+	            calcForce = height - rigidBody.velocity.y;
+                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * (calcForce);
+                rigidBody.AddForce(appliedHoverForce,ForceMode.Acceleration);
+            }
+
+	    }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(destination);
+        }
+    }
+    
+
+}
