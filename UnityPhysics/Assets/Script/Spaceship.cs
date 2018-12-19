453857ea37e8a8328b8fc6b97521aa714df4fa20
Hsantos <igor.hos@gmail.com>
2018-12-19 19:29:26 -0200

    dev

--- UnityPhysics/Assets/Script/Spaceship.cs
+++ UnityPhysics/Assets/Script/Spaceship.cs
@@ -6,10 +6,15 @@ using UnityEngine.Experimental.UIElements;
 public class Spaceship : MonoBehaviour
 {
     public float height = 10f;
+    private float defaultSpeed = 20f;
     public float calcForce;
+    public float calcSpeed;
     public Rigidbody rigidBody;
     public float proportionalHeight;
-    public Vector3 destination;
+    public Vector3 proportionalDestiny;
+    public Vector3 finalDestination;
+    public Vector3 initialDestination;
+    public bool existDestiny;
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -27,21 +32,37 @@ public class Spaceship : MonoBehaviour
 	        if (ht.transform.gameObject.GetComponent<TerrainControl>())
 	        {
 	            proportionalHeight = (height - ht.distance);
-	            calcForce = height - rigidBody.velocity.y;
-                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * (calcForce);
-                rigidBody.AddForce(appliedHoverForce,ForceMode.Acceleration);
+
+	            proportionalDestiny = existDestiny ? (finalDestination - initialDestination) :  Vector3.zero;
+	            proportionalDestiny.y = 0;
+
+                calcForce = height - rigidBody.velocity.y;
+	            calcSpeed = defaultSpeed - rigidBody.velocity.magnitude;
+	            
+                Vector3 appliedHoverForce = (Vector3.up * proportionalHeight * (calcForce)) +  (Vector3.Normalize(proportionalDestiny) * calcSpeed);
+
+
+
+	            rigidBody.AddForce(appliedHoverForce,ForceMode.Acceleration);
             }
-
 	    }
-
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(destination);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name +  " | " +  hit.point);
+                finalDestination = hit.point;
+                initialDestination = transform.position;
+                existDestiny = true;
+            }
         }
     }
     
